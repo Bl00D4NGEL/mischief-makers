7775f972671dd4edf5469efc9e65e3c78f2733b4
Drahsid <Drahsid@gmail.com>
2024-06-01 00:01:58 -0400

    OK some libultra funcs
    - OK pimgr.c
    - OK visetmode.c

---
+++ src/ultralib/io/pimgr.c
@@ -0,0 +1,106 @@
+#include <macros.h>
+#include <PR/os_internal.h>
+#include <PR/ultraerror.h>
+#include "piint.h"
+#include <PR/rdb.h>
+
+// bss
+static OSThread piThread ALIGNED(8);
+static char piThreadStack[OS_PIM_STACKSIZE] ALIGNED(16);
+
+static OSThread ramromThread ALIGNED(8);
+static char ramromThreadStack[1024] ALIGNED(16);
+static OSMesgQueue getRamromQ ALIGNED(8);
+static OSMesg getRamromBuf[1];
+static OSMesgQueue freeRamromQ ALIGNED(8);
+static OSMesg freeRamromBuf[1];
+
+static OSMesgQueue piEventQueue ALIGNED(8);
+static OSMesg piEventBuf[1];
+
+// data
+OSDevMgr __osPiDevMgr = { 0 };
+OSPiHandle* __osPiTable = NULL;
+
+OSPiHandle* __osCurrentHandle[2] ALIGNED(8) = { &CartRomHandle, &LeoDiskHandle };
+
+extern OSPiHandle CartRomHandle;
+extern OSPiHandle LeoDiskHandle;
+
+static void ramromMain(void*);
+
+
+void osCreatePiManager(OSPri pri, OSMesgQueue* cmdQ, OSMesg* cmdBuf, s32 cmdMsgCnt) {
+    u32 savedMask;
+    OSPri oldPri;
+    OSPri myPri;
+
+    if ((pri < OS_PRIORITY_IDLE) || (pri > OS_PRIORITY_MAX)) {
+        __osError(ERR_OSCREATEPIMANAGER, 1, pri);
+        return;
+    }
+
+    if (__osPiDevMgr.active) {
+        return;
+    }
+    osCreateMesgQueue(cmdQ, cmdBuf, cmdMsgCnt);
+    osCreateMesgQueue(&piEventQueue, (OSMesg*)piEventBuf, 1);
+
+    if (!__osPiAccessQueueEnabled) {
+        __osPiCreateAccessQueue();
+    }
+
+    osSetEventMesg(OS_EVENT_PI, &piEventQueue, (OSMesg)0x22222222);
+    oldPri = -1;
+    myPri = osGetThreadPri(NULL);
+
+    if (myPri < pri) {
+        oldPri = myPri;
+        osSetThreadPri(NULL, pri);
+    }
+
+    savedMask = __osDisableInt();
+    __osPiDevMgr.active = 1;
+    __osPiDevMgr.thread = &piThread;
+    __osPiDevMgr.cmdQueue = cmdQ;
+    __osPiDevMgr.evtQueue = &piEventQueue;
+    __osPiDevMgr.acsQueue = &__osPiAccessQueue;
+    __osPiDevMgr.dma = __osPiRawStartDma;
+    __osPiDevMgr.edma = __osEPiRawStartDma;
+    osCreateThread(&piThread, 0, __osDevMgrMain, &__osPiDevMgr, &piThreadStack[OS_PIM_STACKSIZE], pri);
+    osStartThread(&piThread);
+
+    osCreateThread(&ramromThread, 0, ramromMain, NULL, ramromThreadStack + 1024, (OSPri)pri - 1);
+    osStartThread(&ramromThread);
+    __osRestoreInt(savedMask);
+
+    if (oldPri != -1) {
+        osSetThreadPri(NULL, oldPri);
+    }
+}
+
+static void ramromMain(void* arg) {
+    u32 sent;
+    u8 tmp[3];
+
+    osCreateMesgQueue(&getRamromQ, getRamromBuf, 1);
+    osCreateMesgQueue(&freeRamromQ, freeRamromBuf, 1);
+    osSetEventMesg(OS_EVENT_RDB_REQ_RAMROM, &getRamromQ, NULL);
+    osSetEventMesg(OS_EVENT_RDB_FREE_RAMROM, &freeRamromQ, NULL);
+
+    while (TRUE) {
+        osRecvMesg(&getRamromQ, NULL, OS_MESG_BLOCK);
+
+        __osPiGetAccess();
+
+        sent = 0;
+
+        while (sent < 1) {
+            sent += __osRdbSend(tmp, 1, RDB_TYPE_GtoH_RAMROM);
+        }
+
+        osRecvMesg(&freeRamromQ, NULL, OS_MESG_BLOCK);
+
+        __osPiRelAccess();
+    }
+}
