fb99ee272a1a5bed8175f733425e3e78dd8c03db
Drahsid <Drahsid@gmail.com>
2024-06-01 01:58:09 -0400

    OK virtualtophysical.c, OK createmesgqueue.c
    - OK virtualtophysical.c
    - OK createmesgqueue.c
    - split probetlb.s

---
+++ src/ultralib/os/createmesgqueue.c
@@ -0,0 +1,19 @@
+#include <PR/os_internal.h>
+#include <PR/ultraerror.h>
+#include "osint.h"
+
+void osCreateMesgQueue(OSMesgQueue* mq, OSMesg* msg, s32 msgCount) {
+
+    if (msgCount <= 0) {
+        __osError(ERR_OSCREATEMESGQUEUE, 1, msgCount);
+        return;
+    }
+
+    mq->mtqueue = (OSThread*)&__osThreadTail.next;
+    mq->fullqueue = (OSThread*)&__osThreadTail.next;
+    mq->validCount = 0;
+    mq->first = 0;
+    mq->msgCount = msgCount;
+    mq->msg = msg;
+}
+
